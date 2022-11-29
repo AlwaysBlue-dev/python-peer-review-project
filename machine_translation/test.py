dd19eb2e6db86b82963497f0945fd75139192b06
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2022-11-29 14:12:51 +0500

    minor changes

--- machine_translation/test.py
+++ machine_translation/test.py
@@ -7,14 +7,14 @@ class Testtranslator(unittest.TestCase):
     def test1(self):
         # test for the translation of the world ‘Hello’ and ‘Bonjour’.
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertIsNone(english_to_french())
+        self.assertNotEqual(english_to_french('Hello'), None)
 
 
 class TestDouble(unittest.TestCase):
     def test1(self):
         # test for the translation of the world  ‘Bonjour‘ and ‘Hello’.
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertIsNone(french_to_english())
+        self.assertNotEqual(french_to_english('Bonjour'), None)
 
 
 unittest.main()
